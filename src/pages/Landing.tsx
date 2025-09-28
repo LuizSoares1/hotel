7f8653b52ecbb4b272fcb5586af1bd3fc72900d5
Luiz Ernandes <84932111+LuizSoares1@users.noreply.github.com>
2025-09-28 09:02:10 -0300

    dando as caras no forms da landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './style/landing.sass'
+import { CiCalendar } from 'react-icons/ci'
+import { IoIosArrowDown } from 'react-icons/io'
+import { GoPeople, GoTag } from 'react-icons/go'
 
 const Landing: React.FC = () => {
   return (
@@ -9,6 +12,35 @@ const Landing: React.FC = () => {
                 <h1>O MELHOR HOTEL DA REGIÃO</h1>
                 <h2>VENHA NOS VISITAR</h2>
             </div>
+        </div>
+        <div className="landing-form-container">
+            <form className='landing-form'>
+                <div className='landing-form-groups'>
+                    <div className='landing-form-group-date'>
+                        <CiCalendar />
+                        <input type="text"  />
+                        <IoIosArrowDown />
+                    </div>
+                    <div className='landing-form-group-date'>
+                        <CiCalendar />
+                        <input type="text"  />
+                        <IoIosArrowDown />
+                    </div>
+                    <div className='landing-form-group-date'>
+                        <GoPeople />
+                        <input type="text"  />
+                        <IoIosArrowDown />
+                    </div>
+                    <div className='landing-form-group-date'>
+                        <GoTag />
+                        <input type="text"  />
+                        <IoIosArrowDown />
+                    </div>
+                </div>
+                <div className='landing-form-group-btn'>
+                    <button>Reservar</button>
+                </div>
+            </form>
         </div>
     </section>
   )
