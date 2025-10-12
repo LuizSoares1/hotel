d3181be4a5b93f37092f62e3c5391c875f3e50a2
Luiz Ernandes <84932111+LuizSoares1@users.noreply.github.com>
2025-10-11 21:51:10 -0300

    Criação do componente chamado breakfast

---
+++ src/pages/Desktop/breakfast.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style/breakfast.sass'
+
+const CafeDaManha: React.FC = () => {
+  return (
+    <section className="breakfast-section">
+        <div className="breakfast-container">
+
+        </div>
+    </section>
+  )
+}
+
+export default CafeDaManha
