77996ae3a0d51da53384a83c821557bf94089091
Luiz Ernandes <84932111+LuizSoares1@users.noreply.github.com>
2026-09-05 17:18:49 -0300

    melhorias no sass do components desktop e inciando os trabalhos no form.

--- src/pages/Desktop/talkWithUs.tsx
+++ src/pages/Desktop/talkWithUs.tsx
@@ -1,24 +1,30 @@
 import React from "react";
+import './style/talkwithus.sass'
 
 const FaleConosco: React.FC = () => {
     return (
         <section className="talkwithus-section">
             <div className="talkwithus-cobntainer">
-                <div className="talkwith-tittle">
-                    <div className='talkwith-text'>
-                        <div className='talkwith-text-span'>
+                <div className="talkwithus-tittle">
+                    <div className='talkwithus-text'>
+                        <div className='talkwithus-text-span'>
                             <span>Fale conosco</span>
                         </div>
-                        <div className='talkwith-text-tittle'>
-                            <div className="talkwith-text-tittle-h1">
+                        <div className='talkwithus-text-tittle'>
+                            <div className="talkwithus-text-tittle-h1">
                                 <h1>Como podemos te ajudar?</h1>
                             </div>
-                            <div className='talkwith-text-tittle-bar'>
+                            <div className='talkwithus-text-tittle-bar'>
                                 <div className='bar'></div>
                             </div>
                         </div>
                         </div>
                     </div>
+                </div>
+                <div className="talkwithus-form">
+                    <form action="">
+                        
+                    </form>
                 </div>
         </section>
     )
